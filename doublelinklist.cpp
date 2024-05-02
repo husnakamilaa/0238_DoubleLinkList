7d652f03d915c52eec5593122cac6a34bf25a61c
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-05-02 15:45:53 +0700

    menambahkan perintah untuk memasukkan data(nama) ke data field dari node baru

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -16,5 +16,7 @@ void addNode() {
     Node * newNode = new Node (); //step 1 crate a new node
     cout << "\nEnter the roll number of the student : ";
     cin >> newNode->noMhs;      //Assign value to the data field of the new node
+    cout << "\nEnter the name of the student : ";
+    cin >> newNode->name;       //Assign value to the data field of the new node
 }
 
