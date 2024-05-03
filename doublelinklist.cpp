94980b50a1e50255b32895f89208e7b45f181a18
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-05-03 23:26:36 +0700

    menambahkan beberapa kurung kurawa yang tertinggal

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -140,7 +140,9 @@ bool listEmpty()
 void traverse()
 {
     if (listEmpty())
+    {
         cout << "\nList is empty" << endl;
+    }
     else
     {
         cout << "\nRecords in ascending order of roll number are : " <<endl;
@@ -156,14 +158,17 @@ void traverse()
 void revtraverse()
 {
     if (listEmpty())
+    {
         cout << "\nList is empty" << endl;
+    }
     else
     {
         cout << "\nRecords in descending order of roll number are : " << endl;
         Node *currentNode = START;
         while (currentNode->next != NULL)
+        {
             currentNode = currentNode->next;
-        
+        }
         while (currentNode != NULL)
         {
             cout << currentNode->noMhs << " " << currentNode->name << endl;
@@ -184,7 +189,9 @@ void searchData()
     int num;
     cin >> num;
     if (search(num, &prev, &curr) == false)
+    {
         cout << "\nRecord not found" << endl;
+    }
     else
     {
         cout << "\nRecord found" << endl;
