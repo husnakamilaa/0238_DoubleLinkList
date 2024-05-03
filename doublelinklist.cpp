ed2caddc209efe6c332757c0b258fd2e360ffd99
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-05-03 16:50:31 +0700

    membenarkan beberapa kesalahan dan menambahkan comment step 2 yang tertinggal

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -6,21 +6,22 @@ struct Node
 {
     int noMhs;
     string name;
-    Node* next;
-    Node* prev;
+    Node *next;
+    Node *prev;
 };
 
-Node * START = NULL;
+Node *START = NULL;
 
 void addNode() {
-    Node * newNode = new Node (); //step 1 crate a new node
+    Node *newNode = new Node (); //step 1 crate a new node
     cout << "\nEnter the roll number of the student : ";
     cin >> newNode->noMhs;      //Assign value to the data field of the new node
     cout << "\nEnter the name of the student : ";
     cin >> newNode->name;       //Assign value to the data field of the new node
 
     //Insert the new node in the list
-    if (START == NULL || newNode->noMhs == START->noMhs){
+    if (START == NULL || newNode->noMhs == START->noMhs)
+    { // step 2: insert the new node at the beginning
         
         if (START != NULL && newNode->noMhs == START->noMhs) {
             cout << "\033[31mDumplicate roll numbers not allowed\033[0m" << endl;
@@ -35,7 +36,8 @@ void addNode() {
         START = newNode; //step 6: make the new node the first node
 
     }
-    else {
+    else 
+    {
         //insert the new node in the middle or at the end
         Node *current = START;  // step 1.a: start from the first node
         Node *previous = NULL;  // step 1.b: previous node is NULL initially
