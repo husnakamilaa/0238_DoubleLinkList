fd231f9217ab57c1e7370c023f435bb5946bd6d4
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-05-03 15:30:32 +0700

    menambahkan decision making (else) jika previous masih NULL sepertinya new node ini ada di awal list

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -57,6 +57,11 @@ void addNode() {
         {
             previous->next = newNode;   // step 7: make the next field of the previous node point to the new node
         }
+        else
+        {
+            // If previous is still NULL, it mean newNode is now the first node
+            START = newNode;
+        }
     }
 
 }
